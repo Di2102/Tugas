4ac89ace97cdba56c665f750c0d18c7eeb46fa0b
Di2102 <Dinnyamalialestari2102@gmail.com>
2023-01-31 00:08:54 +0700

    demografi

--- app/Views/login.php
+++ app/Views/login.php
@@ -59,8 +59,8 @@
                 <div class="card-body">
 
                   <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+                    <h5 class="card-title text-center pb-0 fs-4">Welcome Admin!</h5>
+                    <p class="text-center small">Silahkan masukkan Username & Password</p>
                   </div>
 
                   <form class="row g-3 needs-validation" method="POST" 
@@ -88,7 +88,7 @@
                       </div>
                     </div>
                     <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Login</button>
+                      <button class="btn btn-primary w-100" type="submit">Silahkan Masuk</button>
                     </div>
                     <div class="col-12">
                       <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
--- app/Views/sidemenu.php
+++ app/Views/sidemenu.php
@@ -53,7 +53,7 @@
             <li>
               <a class="dropdown-item d-flex align-items-center" href="<?= base_url('home/logout');?>">
                 <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
+                <span>Mau Keluar?</span>
               </a>
             </li>
 
